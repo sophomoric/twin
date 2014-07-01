215ffffdcf7c070cacc90d5e93cbe0404b8122dd
Adrian Cann <rebecca_twin@chill.com>
2014-07-01 09:23:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rebecca::Application.config.secret_key_base = '767febc87bb83bdf2542603e5263c29a69188fb0821658396dc53051f4b639ac6a4eb7ab7d0ffd7c40dd11de99a349ce0b9333f5e801d63b6ba8cefed56bb9bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rebecca::Application.config.session_store :cookie_store, key: '_rebecca_session'
